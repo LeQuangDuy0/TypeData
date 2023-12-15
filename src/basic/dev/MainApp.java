e2f100d503e8d4347e15ad006864313b6ca54f7e
KyoDyy <144084641+LeQuangDuy0@users.noreply.github.com>
2023-12-16 02:14:12 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,54 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		System.out.println("THONG TIN MOT SINH VIEN");
+
+		String firstName = "Phan Van";
+		String lastName = "Nam";
+		System.out.println("-ho va ten: " + firstName + " " + lastName);
+
+		String studentCode = "001";
+		System.out.println("-ma sinh vien: " + studentCode);
+
+		String idCard = "123456789";
+		System.out.println("-chung minh nhan dan: " + idCard);
+
+		String className = "23CDTH41";
+		System.out.println("-ten lop: " + className);
+
+		String mediumScore = "28";
+		System.out.println("-diem trung binh: " + mediumScore);
+
+		System.out.println("THONG TIN CONG NHAN");
+
+		String fullName = "Nguyen Van A";
+		System.out.println("-Ho va ten: " + fullName);
+
+		String addRess = "TT hue";
+		System.out.println("-Dia chi: " + addRess);
+
+		String phoNe = "0987654321";
+		System.out.println("-Sdt: " + phoNe);
+
+		String luong = " 10tr";
+		System.out.println("-he so luong: " + luong);
+
+		System.out.println("THONG TIN MOT CHIEC XE");
+
+		String name = "ware";
+		System.out.println("ten: " + name);
+
+		String h = "Honda";
+		System.out.println("Hang: " + h);
+
+		String g = "10tr";
+		System.out.println("gia: " + g);
+
+		String m = "xanh";
+		System.out.println("mau son: " + m);
+
+	}
+
+}
